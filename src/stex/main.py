fd2ce912b11f94d1aaaf172f3811111b53fce2b0
Joezircon <joepalochak@gmail.com>
2026-07-07 19:06:46 -0700

    Fix executable import error

--- src/stex/main.py
+++ src/stex/main.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QApplication
-from .ui.main_window import MainWindow
+from src.stex.ui.main_window import MainWindow
 import sys
+
 
 def main():
     app = QApplication(sys.argv)
@@ -8,6 +9,7 @@ def main():
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
